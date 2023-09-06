669f6692e22f0ecc1b052d795a96686be57bc04b
김예진 <kky156@naver.com>
2023-09-06 13:24:24 +0900

    fix: 좌표 수정

--- ros/catkin_ws/src/ssak3/ssak3/load_map.py
+++ ros/catkin_ws/src/ssak3/ssak3/load_map.py
@@ -35,8 +35,8 @@ class loadMap(Node):
         self.map_size_x = 350
         self.map_size_y = 350
         self.map_resolution = 0.05
-        self.map_offset_x = -8.75
-        self.map_offset_y = -8.75
+        self.map_offset_x = -8.75+1
+        self.map_offset_y = -8.75+4
         self.map_data = [0 for i in range(self.map_size_x*self.map_size_y)]
         grid = np.array(self.map_data)
         grid = np.reshape(grid, (350, 350))
