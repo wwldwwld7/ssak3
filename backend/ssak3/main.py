4d0de446fa37b81abe1f986e82331a014fcd7665
김라현 <wwldwwld7@naver.com>
2023-09-18 17:30:28 +0900

    feat : 마리아디비 연결코드 추가  확인X

--- backend/ssak3/main.py
+++ backend/ssak3/main.py
@@ -1,7 +1,7 @@
+# import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -11,3 +11,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
