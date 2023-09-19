277b9975efda702eb87e52bd018db2fa00ea450f
김라현 <wwldwwld7@naver.com>
2023-09-19 12:58:57 +0900

    feat: 회원가입api [S09P22B201-83]

--- backend/ssak3/main.py
+++ backend/ssak3/main.py
@@ -1,7 +1,11 @@
 # import uvicorn
 from fastapi import FastAPI
 
+from api import auth # auth api가 동작할 수 있도록 main에 추가
+
 app = FastAPI()
+
+app.include_router(auth.router) # auth api가 동작할 수 있도록 main에 추가
 
 @app.get("/")
 async def root():
