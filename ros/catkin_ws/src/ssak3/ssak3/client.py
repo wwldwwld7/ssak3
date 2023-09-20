3bb9806b77af7fbfcf5d35384d22d5a945623abb
김예진 <kky156@naver.com>
2023-09-20 14:17:37 +0900

    fix: 비동기 변경 [S09P22B201-119]

--- ros/catkin_ws/src/ssak3/ssak3/client.py
+++ ros/catkin_ws/src/ssak3/ssak3/client.py
@@ -1,28 +1,36 @@
 import socketio
+import asyncio
 
-# 클라이언트 소켓 생성
-sio = socketio.Client()
-
-
-# 연결
-@sio.event
-def connect():
-    print('connection established')
-
-@sio.event
-def disconnect():
-    print('disconnected from server')
-
-# 데이터 수신 콜백함수
-@sio.on('testcallback')
-def aircon_on(data):
-    print('message received with ', data)
+async def client():
+    # 클라이언트 소켓 생성
+    sio = socketio.AsyncClient()
 
 
-# 서버 연결
-sio.connect('http://127.0.0.1:8000')
+    # 연결
+    @sio.event
+    async def connect():
+        print('connection established')
 
-# 데이터 송신
-sio.emit('test','TEST')
+    @sio.event
+    async def disconnect():
+        print('disconnected from server')
 
-sio.wait()
+    # 데이터 수신 콜백함수
+    @sio.on('testcallback')
+    async def aircon_on(data):
+        print('message received with ', data)
+
+
+    # 서버 연결
+    await sio.connect('http://127.0.0.1:8000')
+
+    # 데이터 송신
+    await sio.emit('test','TEST')
+
+    await sio.wait()
+
+def main(args = None):
+    asyncio.run(client())
+
+if __name__ == "__main__":
+    main() 
