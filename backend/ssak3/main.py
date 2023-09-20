c6e0620b05ec3843004635f4750d90bb1ca594f7
김예진 <kky156@naver.com>
2023-09-20 12:59:53 +0900

    feat:BE socket 구현[S09P22B201-106]

--- backend/ssak3/main.py
+++ backend/ssak3/main.py
@@ -1,13 +1,23 @@
+
 from fastapi import FastAPI
+import uvicorn
+from sockets import sio_app
 
 app = FastAPI()
 
+app.mount("/", app = sio_app)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"test": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+
+    # main.py를 실행할 때만 FastAPI 서버를 시작
+    uvicorn.run('main:app', reload = True)
