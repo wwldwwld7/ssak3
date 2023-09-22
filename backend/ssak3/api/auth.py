d17dfa2ae799c2a5e136f79a86a60a26401bb646
오다희 <dhekgml1234@gmail.com>
2023-09-22 14:23:00 +0900

    test: requirement.txt에 pymysql 추가

--- backend/ssak3/api/auth.py
+++ backend/ssak3/api/auth.py
@@ -1,19 +1,19 @@
-# from fastapi import APIRouter
-# from fastapi import Depends
-# from sqlalchemy.orm import Session
-# from starlette import status
+from fastapi import APIRouter
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from starlette import status
 
-# from models.auth import auth
-# from db.db import session
+from models.auth import auth
+from db.db import session
 
-# router = APIRouter(prefix="/auth")
+router = APIRouter(prefix="/auth")
 
-# @router.post("/sign-up", status_code=status.HTTP_200_OK)
-# def signUp(id: str, name: str, password: str):
-#     db = session()
-#     try:
-#         user = auth(id=id, name=name, password=password)
-#         db.add(user)
-#         db.commit()
-#     finally:
-#         db.close()
+@router.post("/sign-up", status_code=status.HTTP_200_OK)
+def signUp(id: str, name: str, password: str):
+    db = session()
+    try:
+        user = auth(id=id, name=name, password=password)
+        db.add(user)
+        db.commit()
+    finally:
+        db.close()
--- backend/ssak3/main.py
+++ backend/ssak3/main.py
@@ -1,21 +1,21 @@
 # import uvicorn
 from fastapi import FastAPI
 
-# from api import auth # auth api가 동작할 수 있도록 main에 추가
+from api import auth # auth api가 동작할 수 있도록 main에 추가
 
 app = FastAPI()
 
-# app.include_router(auth.router) # auth api가 동작할 수 있도록 main에 추가
+app.include_router(auth.router) # auth api가 동작할 수 있도록 main에 추가
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
 
-@app.get("/hello")
-def hello():
-    return {"message": "DB sqlalchemy인가 뭐시기 하는 에러가 뜨는데 쉬불탱 못해먹겠드아..."}
+# @app.get("/hello")
+# def hello():
+#     return {"message": "DB sqlalchemy인가 뭐시기 하는 에러가 뜨는데 쉬불탱 못해먹겠드아..."}
