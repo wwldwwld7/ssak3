e624dfad86502d4f30dc819c6b6724be7744d4c1
오다희 <dhekgml1234@gmail.com>
2023-09-22 13:58:41 +0900

    test: db 지우고 빌드

--- backend/ssak3/models/auth.py
+++ backend/ssak3/models/auth.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+# from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+# from sqlalchemy.orm import relationship
 
-from db.db import Base
+# from db.db import Base
 
 
-class auth(Base):
-    __tablename__ = "auth"
+# class auth(Base):
+#     __tablename__ = "auth"
 
-    auth_id = Column(Integer, primary_key=True, autoincrement=True)
-    id = Column(String, unique=True,  nullable=False)
-    name = Column(String, nullable=False)
-    password = Column(String, nullable=False)
+#     auth_id = Column(Integer, primary_key=True, autoincrement=True)
+#     id = Column(String, unique=True,  nullable=False)
+#     name = Column(String, nullable=False)
+#     password = Column(String, nullable=False)
 
