b3540a7fc53650d221a463d15e9bfa3b26fea82e
오다희 <dhekgml1234@gmail.com>
2023-09-22 14:38:56 +0900

    delete: .idea폴더 삭제

--- backend/ssak3/main.py
+++ backend/ssak3/main.py
@@ -15,7 +15,3 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
-
-# @app.get("/hello")
-# def hello():
-#     return {"message": "DB sqlalchemy인가 뭐시기 하는 에러가 뜨는데 쉬불탱 못해먹겠드아..."}
