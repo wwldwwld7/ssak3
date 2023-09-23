03d5fa0b1cbd99dce53fa346da5937fb2db3781c
김라현 <wwldwwld7@naver.com>
2023-09-23 18:27:39 +0900

    fix : 시리얼넘버 등록할 때 사용자 아이디로 받게 수정

--- backend/ssak3/api/robot.py
+++ backend/ssak3/api/robot.py
@@ -12,13 +12,16 @@ from db.db import get_db
 
 router = APIRouter(prefix="/robot")
 @router.post("/regist", status_code=status.HTTP_200_OK)
-def robotRegist(auth_id: str, number: str, db: Session = Depends(get_db)):
+def robotRegist(id: str, number: str, db: Session = Depends(get_db)):
     try:
-        exist_user = db.query(auth).filter(auth.auth_id == auth_id).first()
+        if number is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="입력을 확인하세요.")
+        exist_user = db.query(auth).filter(auth.id == id).first()
         if not (exist_user):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail="존재하지 않는 사용자 입니다.") # 아이디가 존재하지 않으면 예외 던지기
-        robot = turtlebot(auth_id=auth_id, serial_number=number)
+        robot = turtlebot(auth_id=exist_user.auth_id, serial_number=number)
         db.add(robot)
         db.commit()
     finally:
