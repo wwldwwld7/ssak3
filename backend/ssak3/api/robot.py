75ca36f4a0f6d94e62af5d6fd39278aaf3f5d396
김라현 <wwldwwld7@naver.com>
2023-09-24 18:56:09 +0900

    feat: 로그 전부 ê°가져오기api

--- backend/ssak3/api/robot.py
+++ backend/ssak3/api/robot.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from starlette import status
+from datetime import datetime
 
 from models.auth import auth
 from models.tutlebot import turtlebot
+from models.get import get
 from db.db import get_db
 
 router = APIRouter(prefix="/robot")
@@ -22,3 +24,22 @@ def robotRegist(id: str, number: str, db: Session = Depends(get_db)):
         db.commit()
     finally:
         db.close()
+
+@router.get("/log", status_code=status.HTTP_200_OK)
+def logAll(id:str, db: Session = Depends(get_db)):
+    exist_user = db.query(auth).filter(auth.id == id).first()
+    if not exist_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="존재하지 않는 사용자 입니다.")  # 아이디가 존재하지 않으면 예외 던지기
+    logs = db.query(get).filter(get.auth_id == exist_user.auth_id).all()
+    return logs
+
+@router.post("/log-regist", status_code=status.HTTP_200_OK) # 실험용으로 만들어 본 것
+def registlog(id:str, db: Session = Depends(get_db)):
+    exist_user = db.query(auth).filter(auth.id == id).first()
+    current_time = datetime.now()
+    formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+    log = get(auth_id=exist_user.auth_id, start_time=formatted_time, end_time=formatted_time, laundry_cnt=10)
+    db.add(log)
+    db.commit()
+    db.close()
