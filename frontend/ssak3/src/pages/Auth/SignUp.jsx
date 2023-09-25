8717db1cce5affd3aa9943c5e52f51bf8e363aff
이정섭 <dark6ro@naver.com>
2023-09-26 01:01:21 +0900

    feat: sign-up axios 추가 [S09P22B201-158]

--- frontend/ssak3/src/pages/Auth/SignUp.jsx
+++ frontend/ssak3/src/pages/Auth/SignUp.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 import "./LoginStyles.css";
 
 const SignUp = ( ) =>{
+    const instance = axios.create({
+        baseURL: 'https://j9b201.p.ssafy.io/auth',
+        headers: { 'Content-type': 'application/json' },
+    });
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState(
+        {
+            id: '',
+            name: '',
+            password: ''
+        }
+    );
+    const onSubmit = async (e) => {
+        try{
+            const res = await instance.post('/sign-up', formData);
+            console.log('회원가입 성공!', res.data);
+            navigate('/');
+
+        } catch(err){
+            window.alert("회원가입 실패 : "+err);
+        }
+    }
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({ ...formData, [name]: value});
+    };
     return (
     <div className="container">
         <div className="signupheader">
             <p className="signupTitle">회원가입</p>
             <p className="signupTitleLabel">회원정보를 입력하세요</p>
         </div>
-        <form className="signupForm">
+        <form className="signupForm" onSubmit={onSubmit}>
             <div className="signupFormarea">
                 <div>
                     <label for="username">이름</label><br/>
-                    <input type="text" id="username" name="username" placeholder="name" required/>
+                    <input type="text" id="name" name="name" placeholder="name" onChange={handleChange} required/>
                 </div>
                 <div>
                     <label for="userid">아이디</label><br/>
-                    <input type="text" id="userid" name="userid" placeholder="ID" required/>
+                    <input type="text" id="id" name="id" placeholder="ID" onChange={handleChange} required/>
                 </div>
                 <div>
                     <label for="password">비밀번호</label><br/>
-                    <input type="password" id="password" name="password" placeholder="Password" required/>
+                    <input type="password" id="password" name="password" placeholder="Password" onChange={handleChange} required/>
                     <p>비밀번호는 반드시 6자 이상이어야 합니다.</p>
                 </div>
             </div>
