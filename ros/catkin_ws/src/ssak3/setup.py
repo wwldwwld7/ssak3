918bb0360005bae43158bf0087d1fe3e11c2a019
김예진 <kky156@naver.com>
2023-09-25 15:50:06 +0900

    feat: socket client & 환경송신[S09P22B201-121]

--- ros/catkin_ws/src/ssak3/setup.py
+++ ros/catkin_ws/src/ssak3/setup.py
@@ -30,7 +30,8 @@ setup(
             'point_pub = ssak3.point_pub:main',
             'up_object = ssak3.up_object:main',
             'slam = ssak3.slam:main',
-            'client = ssak3.client:main'
+            'client = ssak3.client:main',
+            'turtlebot_status = ssak3.turtlebot_status:main'
         ],
     },
 )
