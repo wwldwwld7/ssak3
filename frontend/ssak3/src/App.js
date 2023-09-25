d81de7ac92a63e32561909e93834c342ba2dda38
홍영기 <hongyk9885@gmail.com>
2023-09-25 10:54:35 +0900

    rename : front 폴더 구조 수정

--- frontend/ssak3/src/App.js
+++ frontend/ssak3/src/App.js
@@ -1,26 +1,25 @@
 // import React, { useState } from "react";
 // import TurtlebotController from "./components/turtlebotcontroller/index.jsx";
 import './App.css';
-import TurtlebotStarter from "./components/turtlebotstarter/index.jsx";
-import TurtlebotMain from "./components/turtlebotMain/index.jsx";
-import TurtlebotLog from "./components/turtlebotLog/index.jsx"
-import SignUp from './pages/Auth/SignUp';
-import Login from "./pages/LogIn.jsx";
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AddSchedule from './pages/Schedule/AddSchedule';
-import AddTuttle from './pages/AddTuttle';
+import TurtleBotStarter from './pages/Control';
+import Login from './pages/Auth/LogIn';
+import Signup from './pages/Auth/SignUp';
+import TurtlebotController from './pages/Main';
+import TurtleBotLog from './pages/Log';
+
 
 function App() {
   return (
   <>
     <Router>
       <Routes>
-        <Route path="/" element={<TurtlebotStarter/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="signup" element={<SignUp/>} />
-        <Route path="addschedule" element={<AddSchedule/>} />
-        <Route path="addtuttle" element={<AddTuttle/>} />
+        <Route path="/" element={<Login/>} />
+        <Route path="signup" element={<Signup/>} />
+        <Route path="main" element={<TurtlebotController/>} />
+        <Route path="start" element={<TurtleBotStarter/>} />
+        <Route path="log" element={<TurtleBotLog/>} />
       </Routes>
     </Router>
   </>
