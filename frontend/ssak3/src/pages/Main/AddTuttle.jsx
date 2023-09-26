e066a330d1e518dd55e3030b1ea7ed3e477d24a4
홍영기 <hongyk9885@gmail.com>
2023-09-26 15:23:28 +0900

    feat : 터틀봇 등록 api 추가 [S09P22B201-180]

--- frontend/ssak3/src/pages/Main/AddTuttle.jsx
+++ frontend/ssak3/src/pages/Main/AddTuttle.jsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Cookies } from 'react-cookie';
+import { defaultInstance as api } from '../../util/token.jsx';
 
 const AddTuttle = () => {
+    const navigate = useNavigate();
+
+    const GoMain = () => {
+        navigate("/main");
+    };
+
+    const [inputTurtle, setInputTurtle] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputTurtle(event.target.value);
+    };
+
+    const formdata = {
+        "id" : "test",
+        "serial_number" : inputTurtle
+    }
+
+    const sendRegister = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await api.post("/robot/regist", formdata);
+            console.log('등록성공', response.data);
+            navigate('/main');
+        } catch (error) {
+            console.error(error);
+            setInputTurtle('');
+            alert("오류가 발생했습니다.");
+        }
+    };
+
     return(
         <>
             <div style={{position:'absolute',width:'390px', height:'812px'}}>
@@ -17,12 +49,12 @@ const AddTuttle = () => {
                 </div>
                 <div style={{width:'100%', height: '40%'}}>
                     <div style={{width:'100%', height: '50%', display:'flex', justifyContent:'center',alignItems:'center'}}>
-                        <input type="text" id="serial" name="serial" placeholder="Serial Number" required/>
+                        <input type="text" value={inputTurtle} onChange={handleInputChange} placeholder="Serial Number" required/>
                     </div>
                     <div style={{width:'100%', height: '50%', display:'flex', justifyContent:'center',alignItems:'start'}}>
                         <div style={{width:'325px', height: '30%',display:'flex', justifyContent:'space-between'}}>
-                            <button style={{width:'150px', height: '50px',border:'1px solid #ededed',backgroundColor:'#fafafa', borderRadius:'10px',fontFamily:'inherit',color:'#838589'}}>취소</button>
-                            <button style={{width:'150px', height: '50px',border:'1px solid #ededed',backgroundColor:'#fafafa', borderRadius:'10px',fontFamily:'inherit',color:'#838589'}}>등록하기</button>
+                            <div onClick={GoMain} style={{width:'150px', height: '50px',border:'1px solid #ededed', display:'flex', justifyContent:'center',alignItems:'center',backgroundColor:'#fafafa', borderRadius:'10px',fontFamily:'inherit',color:'#838589', cursor:'pointer'}}>취소</div>
+                            <div onClick={sendRegister} style={{width:'150px', height: '50px',border:'1px solid #ededed', display:'flex', justifyContent:'center',alignItems:'center',backgroundColor:'#fafafa', borderRadius:'10px',fontFamily:'inherit',color:'#838589', cursor:'pointer'}}>등록하기</div>
                         </div>
                     </div>
                 </div>
