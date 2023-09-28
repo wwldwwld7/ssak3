052eda4b038fa91475630697d8e991de00351c80
오다희 <dhekgml1234@gmail.com>
2023-09-28 18:26:22 +0900

    refactor: 로그 저장 최적화 및 transaction 적용

--- backend/ssak3/api/run.py
+++ backend/ssak3/api/run.py
@@ -13,27 +13,29 @@ from db.db import get_db
 
 router = APIRouter(prefix="/run")
 
-class Run(BaseModel):
+class Start(BaseModel):
     id: str
     laundryList: List[int]
 
 
 # 세탁물 주행시 선택되는 것들 반영해서 생성
 @router.post("/start", status_code=status.HTTP_200_OK)
-def registrun(run: Run, db: Session = Depends(get_db)):
+def registrun(start: Start, db: Session = Depends(get_db)):
     # 유저 확인
-    exist_user = db.query(auth).filter(auth.id == run.id).first()
+    exist_user = db.query(auth).filter(auth.id == start.id).first()
 
     if not (exist_user):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="존재하지 않는 사용자 입니다.")
-    
-    # laundryList에 있는 값이 데이터 베이스에 잘 들어가 있는지 확인
-    for laundry_id in run.laundryList:
-        laundry_item = db.query(laundry).filter(laundry.laundry_id==laundry_id).first()
+
+    laundry_ids = start.laundryList
+    laundry_items = db.query(laundry).filter(laundry.laundry_id.in_(laundry_ids)).all()
+
+    for laundry_id in laundry_ids:
+        laundry_item = next((item for item in laundry_items if item.laundry_id == laundry_id), None)
         if not laundry_item:
             raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
-                                detail="존재하지 않는 세탁물입니다. index가 아닌 id값으로 잘 넣었는지 확인 바람")
+                                detail=f"{laundry_id}번 세탁물은 존재하지 않는 세탁물입니다. index가 아닌 id값으로 잘 넣었는지 확인 바람")
 
     # 아니면 어짜피 동작중에는 실행을 못하니깐 생각해보면, auth_id에서 제일 최근 주행로봇의 end_time만 확인하면 될것같음
     first = db.query(get).filter(get.auth_id == exist_user.auth_id).order_by(get.get_id.desc()).first()
@@ -44,23 +46,74 @@ def registrun(run: Run, db: Session = Depends(get_db)):
 
     # 이제 로봇이 실행중이 아니거나, 첫사용자임
 
-    # 1. get만들기 사용자번호, 시작시간만 넣어주기
-    get_item = get(auth_id=exist_user.auth_id, start_time=datetime.now())
-    db.add(get_item)
-    db.commit()
+    try:
+        # 1. get만들기 사용자번호, 시작시간만 넣어주기
+        get_item = get(auth_id=exist_user.auth_id, start_time=datetime.now())
+        db.add(get_item)
+        db.flush()
 
-    get_id = get_item.get_id
+        get_id = get_item.get_id
 
-    # 2. request로 들어온 laundry_id로 select에다가 넣어주기
-    for laundry_id in run.laundryList:
-        select_item = select(get_id=get_id, laundry_id=laundry_id)
-        db.add(select_item)
-    db.commit()
+        # 2. request로 들어온 laundry_id로 select에다가 넣어주기
+        for laundry_id in start.laundryList:
+            select_item = select(get_id=get_id, laundry_id=laundry_id)
+            db.add(select_item)
+
+        db.commit()
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Database error: {str(e)}")
+
+    finally:
+        db.close()
+
+    return get_id
 
 
+class Ros(BaseModel):
+    index: int
+    name: str
+    cnt: int
 
+class End(BaseModel):
+    id: str
+    get_id: int
+    laundries: List[Ros]
 
-
+# 주행 완료시 모든 데이터베이스 등록
+# @router.patch("/end", status_code=status.HTTP_200_OK)
+# def updaterun(end: End, db: Session = Depends(get_db)):
+#     # 유저 확인
+#     exist_user = db.query(auth).filter(auth.id == end.id).first()
+#
+#     if not (exist_user):
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail="존재하지 않는 사용자 입니다.")
+#
+#     # laundryList에 있는 값이 데이터 베이스에 잘 들어가 있는지 확인
+#     for ros in end.laundries:
+#         laundry_item = db.query(laundry).filter(laundry.laundry_ros == ros.name).first()
+#         if not laundry_item:
+#             raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
+#                                 detail="존재하지 않는 세탁물입니다. ros에서 받아오는 이름과 같지 않습니다.")
+#
+#     get_item = db.query(get).filter(get.get_id == end.get_id).first()
+#     if len(get_item) == 0:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail="해당 로그가 존재하지 않습니다.")
+#
+#     if (get_item.end_time):
+#         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
+#                             detail="벌써 실행 완료 된 로그입니다. 다시 한번 확인해주세요.")
+#
+#
+#     current_time = datetime.now()
+#     formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+#
+#     get_item.end_time = formatted_time
+#
 
 
 
