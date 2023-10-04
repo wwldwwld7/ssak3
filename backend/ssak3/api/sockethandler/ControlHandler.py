2c07957c6bab89cdfd368e7c5ab2e0355ce6fc77
김예진 <kky156@naver.com>
2023-10-04 12:48:31 +0900

    fix: msg 수정

--- backend/ssak3/api/sockethandler/ControlHandler.py
+++ backend/ssak3/api/sockethandler/ControlHandler.py
@@ -22,13 +22,12 @@ class ControlHandler(socketio.AsyncNamespace):
 
     # 세탁물 정보 확인
     async def on_result(self, sid, data):
-        # 결과가 json으로 들어온다고 가정함
         from api.run import updaterun
         from api.run import Ros
         from api.run import End
         # 총 개수와 터틀봇 번호로 들어왔다면 (String으로)
         turtlebot_no = data['turtlebot_no']
-
+        result_list = data['result']
         # ros 응답 형태 확인 후 수정해야 함.
         # ros에는 name : cnt 형태로 보내야 할듯
 
@@ -40,7 +39,7 @@ class ControlHandler(socketio.AsyncNamespace):
             end.id = user
 
             # 응답 확인 후 수정
-            end.laundries.append(Ros('skirts', data['skirts']))
-            end.laundries.append(Ros('pants', data['pants']))
+            end.laundries.append(Ros('shirts', result_list['shirts']))
+            end.laundries.append(Ros('pants', result_list['pants']))
 
             updaterun(end)
