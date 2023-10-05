530104286a8a2b1ebc1dad852d7c9bce4526a6d6
김예진 <kky156@naver.com>
2023-10-05 11:16:52 +0900

    fix: 작동 완료 로그 수정

--- backend/ssak3/api/sockethandler/ControlHandler.py
+++ backend/ssak3/api/sockethandler/ControlHandler.py
@@ -1,16 +1,23 @@
+import httpx
 import socketio
 
-from fastapi import Depends
 from sqlalchemy.orm import Session
 from db.db import get_db
 
 from models.turtlebot import turtlebot
+from models.auth import auth
 
+# authid 구하기
+def finduser(num: str):
+    db: Session = next(get_db())
+    user = db.query(turtlebot).filter(turtlebot.serial_number == num).first()
+    return user.auth_id
 
-def findUser(num: int, db: Session = Depends(get_db)):
-    user = db.query(turtlebot).filter(turtlebot.turtlebot_id == num).first()
-    return user
-
+# 유저 id 구하기
+def findid(num: int):
+    db: Session = next(get_db())
+    id = db.query(auth).filter(auth.auth_id == num).first()
+    return id.id
 
 class ControlHandler(socketio.AsyncNamespace):
 
@@ -22,24 +29,29 @@ class ControlHandler(socketio.AsyncNamespace):
 
     # 세탁물 정보 확인
     async def on_result(self, sid, data):
-        from api.run import updaterun
-        from api.run import Ros
-        from api.run import End
-        # 총 개수와 터틀봇 번호로 들어왔다면 (String으로)
-        turtlebot_no = data['turtlebot_no']
+        operate_no = data['operateNo']
+        serial_no = data['serialNo']
         result_list = data['result']
-        # ros 응답 형태 확인 후 수정해야 함.
-        # ros에는 name : cnt 형태로 보내야 할듯
 
-        user = findUser(turtlebot_no)
+        user = finduser(str(serial_no))
+        id = findid(user)
 
         if (user):
-            end = End()
-            end.get_id = sid
-            end.id = user
+            result = []
+            result.append({"name":"shirts", "cnt":int(result_list[0])})
+            result.append({"name":"pants", "cnt":int(result_list[1])})
 
             # 응답 확인 후 수정
-            end.laundries.append(Ros('shirts', result_list['shirts']))
-            end.laundries.append(Ros('pants', result_list['pants']))
+            # 로컬
+            # url = "http://127.0.0.1:8000/run/end"
 
-            updaterun(end)
+            # 서버
+            url = "https://https://j9b201.p.ssafy.io/api/run/end"
+
+            async with httpx.AsyncClient() as client:
+                response = await client.patch(url, json = {"id":id, "get_id":operate_no, "laundries":result})
+                
+                if response.status_code == 200:
+                    print("동작 저장")
+                else:
+                    print("동작 저장 실패")
