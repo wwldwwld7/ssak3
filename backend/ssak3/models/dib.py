f998d1b3e88794186144f068e96264b605713f3b
김라현 <wwldwwld7@naver.com>
2023-11-27 17:46:42 +0900

    update gitattributes for lfs

--- backend/ssak3/models/dib.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
-from db.db import Base
-
-class dib(Base):
-    __tablename__ = "dib"
-
-    dib_id = Column(Integer, primary_key=True, autoincrement=True)
-    auth_id = Column(Integer, ForeignKey("auth.auth_id"), nullable=False)
-    laundry_id = Column(Integer, ForeignKey("laundry.laundry_id"), nullable=False)
--- frontend/ssak3/src/components/test/TestComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TestComponent = () =>{
-    return <div>TEST</div>
-};
-
-export default TestComponent;
